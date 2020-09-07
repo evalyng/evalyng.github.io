ede3d0c04cc723c87d029a897a3175ed38688c69
evalyng <70808973+evalyng@users.noreply.github.com>
2020-09-07 07:36:55 -0600

    Update order.js

--- js/order.js
+++ js/order.js
@@ -4,7 +4,7 @@ document.querySelector('button').onclick = () => {
 	setTimeout(() => {
 		new Audio('media/internyet.mp3').play().loop = true
 
-		alert('Error id: ' + Math.random().toString().split('.')[1] + ' 您的連接被大中國防火牆中斷，原因：操您 pls try again?')
+		alert('Error id: ' + Math.random().toString().split('.')[1] + ' 您的連接被大中國防火牆中斷，原因：操您 pls try againe?')
 
 		const _ = document.createElement('link')
 		_.rel = 'stylesheet'
