94a8b27b59bb67426762195ef0035f34473e3be6
evalyng <70808973+evalyng@users.noreply.github.com>
2020-09-07 08:17:35 -0600

    Update order.js

--- js/order.js
+++ js/order.js
@@ -1,15 +1,17 @@
-document.querySelector('button').onclick = () => {
-	document.body.style.cursor = 'wait'
+document.querySelectorAll('button').forEach(button => {
+	button.onclick = () => {
+		document.body.style.cursor = 'wait'
 
-	setTimeout(() => {
-		new Audio('media/internyet.mp3').play().loop = true
+		setTimeout(() => {
+			new Audio('media/internyet.mp3').play().loop = true
 
-		alert('Error id: ' + Math.random().toString().split('.')[1] + ' 您的連接被大中國防火牆中斷，原因：操您 pls try againe?')
+			alert('Error id: ' + Math.random().toString().split('.')[1] + ' 您的連接被大中國防火牆中斷，原因：操您 pls try againe?')
 
-		const _ = document.createElement('link')
-		_.rel = 'stylesheet'
-		_.href = 'css/360.css'
+			const _ = document.createElement('link')
+			_.rel = 'stylesheet'
+			_.href = 'css/360.css'
 
-		document.head.append(_)
-	}, 2000)
-}
+			document.head.append(_)
+		}, 2000)
+	}
+})
