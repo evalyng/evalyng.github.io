e31f6620738892ff8a9daa0a26cf24fd86a68243
evalyng <70808973+evalyng@users.noreply.github.com>
2020-09-06 20:50:47 -0600

    Add files via upload

---
+++ js/order.js
@@ -0,0 +1,15 @@
+document.querySelector('button').onclick = () => {
+	document.body.style.cursor = 'wait'
+
+	setTimeout(() => {
+		new Audio('media/internyet.mp3').play().loop = true
+
+		alert('Error id: ' + Math.random().toString().split('.')[1] + ' 您的連接被大中國防火牆中斷，原因：操您 pls try again?')
+
+		const _ = document.createElement('link')
+		_.rel = 'stylesheet'
+		_.href = 'css/_.css'
+
+		document.head.append(_)
+	}, 2000)
+}
